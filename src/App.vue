88170ed00c5f94b5abfe90d9952228522f9753d2
34T5 <5087844+H2z2H@user.noreply.gitee.com>
2024-11-19 13:08:55 +0800

    docs: :memo: 复现步骤

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import {useRequest} from "alova/client";
 const { send, data } = useRequest(() => getWeather({name:'北京市'}), {
   immediate: true,
 })
-
+// 请求完成后点击change按钮改变data的某个属性后再次send请求，会发现缓存被修改，缓存日志也有体现
 function onchange() {
   data.value.city = '123'
 }
